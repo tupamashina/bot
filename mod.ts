07355fb4a1c4697b039dd7215d9dae70d425452f
Nikita Naleikin <nnaleikin361@gmail.com>
2022-11-03 20:14:51 +0400

    Fixes TypeError: Deno.readFileSync is not a function

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 import { serve } from 'https://deno.land/std@0.159.0/http/server.ts';
 import { bot } from './bot.ts';
 import { grammy } from './deps.ts';
-import { loadEnv } from './env.ts';
 
 // ============== Патч для правильной работы json'а с bigint'ами ==============
 const { stringify: originalStringify, parse: originalParse } = JSON;
@@ -19,29 +19,21 @@ JSON.parse = (text) =>
   );
 // ============================================================================
 
-const isDev = Deno.env.get('ENV') === 'development';
-loadEnv(isDev);
+const handleUpdate = grammy.webhookCallback(bot, 'std/http');
 
-if (isDev) {
-  bot.catch(console.error);
-  bot.start();
-} else {
-  const handleUpdate = grammy.webhookCallback(bot, 'std/http');
-
-  serve((req) => {
-    if (req.method === 'POST') {
-      if (new URL(req.url).pathname.slice(1) === bot.token) {
-        try {
-          return handleUpdate(req);
-        } catch (error) {
-          console.error(error);
-        }
+serve((req) => {
+  if (req.method === 'POST') {
+    if (new URL(req.url).pathname.slice(1) === bot.token) {
+      try {
+        return handleUpdate(req);
+      } catch (error) {
+        console.error(error);
       }
     }
+  }
 
-    return new Response();
-  }, {
-    onListen: ({ hostname, port }) =>
-      console.info(`Server is listening on ${hostname}:${port}`),
-  });
-}
+  return new Response();
+}, {
+  onListen: ({ hostname, port }) =>
+    console.info(`Server is listening on ${hostname}:${port}`),
+});
