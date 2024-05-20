ed6719210f5fbb2a420e4bb5bd1ef2635ca790c2
Nikita Naleikin <nnaleikin361@gmail.com>
2024-05-20 18:03:42 +0400

    Fixes

--- prisma/mod.ts
+++ prisma/mod.ts
@@ -1,3 +1,6 @@
 import { prisma } from "../deps.ts";
+import { withAccelerate } from "npm:@prisma/extension-accelerate@1.0.0";
 
-export const prismaClient = new prisma.PrismaClient();
+export const prismaClient = new prisma.PrismaClient().$extends(
+  withAccelerate(),
+);
